a618cae92e9c0943557dbc6341da57ec25b5d80e
Guilherme <guilherme.ramos09@gmail.com>
2019-05-28 23:30:20 -0300

    Integrated technologies model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,10 +1,9 @@
 class Portfolio < ApplicationRecord
-  include Placeholder
   has_many :technologies
   accepts_nested_attributes_for :technologies,
                                 reject_if: lambda { |attrs| attrs['name'].blank? }
 
-  include Placeholder
+  
   validates_presence_of :title, :body, :main_image, :thumb_image
 
   def self.angular
@@ -13,10 +12,10 @@ class Portfolio < ApplicationRecord
 
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
 
-  after_create :set_defaults
+  after_initialize :set_defaults
 
   def set_defaults
-    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
-    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
+    self.main_image = "https://placehold.it/600x400"
+    self.thumb_image = "https://placehold.it/350x200"
   end
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,10 +1,10 @@
 class Skill < ApplicationRecord
-  include Placeholder
+  
   validates_presence_of :title, :percent_utilized
 
   after_create :set_defaults
 
   def set_defaults
-    self.badge ||= Placeholder.image_generator(height: '250', width: '250')
+    self.badge = "https://placehold.it/250x250"
   end
 end
